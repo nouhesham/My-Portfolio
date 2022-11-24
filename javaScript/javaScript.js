69bc3579083775ce432b004d741ae6556bdf2f7a
Nourhan <nourhanhesham263@gmail.com>
2022-11-25 00:00:53 +0200

    adding new code

--- javaScript/javaScript.js
+++ javaScript/javaScript.js
@@ -1,5 +1,27 @@
 "strict";
+const header = document.querySelector("#header");
+const icon = document.querySelector("#icon1");
+const main = document.querySelector(".Main");
+let x = 0;
+console.log(x);
+const again = function () {
+  x = x === 0 ? 1 : 0;
+  // console.log(x);
+};
 
-const icon = document.querySelector(".icon");
+const bright = function () {
+  // console.log(x);
+  if (x === 0) {
+    main.style.backgroundImage = "url(images/second.jpeg)";
+    header.style.backgroundColor = "lightblue";
+    console.log(main.style);
+  } else if (x === 1) {
+    main.style.backgroundImage = "url(images/firstphoto.jpeg)";
+    header.style.backgroundColor = "purple";
+    console.log(x);
+  }
+  again();
+  // console.log(x);
+};
 
-const bright = function () {};
+document.getElementById("icon1").addEventListener("click", bright);
